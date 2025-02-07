926c601830820856ec9da05e480dba9cf020dbff
Daniel Kosmin <kosmindaniel@gmail.com>
2025-02-07 10:27:46 -0500

    pushing

---
+++ src/main/java/com/kosmin/datavis/DatavisApplication.java
@@ -0,0 +1,13 @@
+package com.kosmin.datavis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatavisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatavisApplication.class, args);
+	}
+
+}
